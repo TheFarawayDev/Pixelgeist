37739de21a80a7e20c3e4c28637fa576a27c8a2c
Wolfdevproductions99 <gavingiancola@yahoo.com>
2025-11-13 08:38:22 -0500

    Organise
    Self explanatory, can you make the movement work plz :)

---
+++ pixelgeist/Scripts/character.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	#I've tried to make the movement work so much i'm leaving that to u :)
