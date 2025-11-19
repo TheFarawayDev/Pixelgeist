6c2d50577a99febaa8008a1d48b8717267a95f32
9G4N9 <gavingiancola@yahoo.com>
2025-11-19 16:06:42 -0500

    corruption fix

--- pixelgeist/Scenes/startscreen.gd
+++ pixelgeist/Scenes/startscreen.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+<<<<<<< Updated upstream
 	pass # Replace with function body.
 
 
@@ -10,3 +11,10 @@ func _ready() -> void:
 
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://Scenes/test-map.tscn")
+=======
+	if timesplayed <1:
+		mainmenuanimation.play("default")
+	await mainmenuanimation.animation_finished
+	$VideoStreamPlayer.paused = false
+	timesplayed =1
+>>>>>>> Stashed changes
